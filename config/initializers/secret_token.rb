a57725baed915d3fd87cc59fa9914798ebc48d27
John Ashmead <john.ashmead@ashmeadsoftware.com>
2013-06-11 13:56:33 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Talkvite::Application.config.secret_key_base = 'abd56dbc28249e8158d52bd82f00fb1c240e3a9891eadb78af4b41e52271200a3b74908862a8c7fe85842dc9298440ffd7fafd5c7967bf51c0d9e33f3e72abcf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Talkvite::Application.config.session_store :cookie_store, key: '_talkvite_session'
