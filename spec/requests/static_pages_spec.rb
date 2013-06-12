cd8b623091dac47cbb6059c17eadafb8f83c8642
jashmead <john.ashmead@ashmeadsoftware.com>
2013-06-12 13:35:44 -0400

    added contact page; commented out pending tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  
+  let(:base_title) { "TalkVite" }
 
   describe "Home page" do
 
@@ -40,4 +42,18 @@ describe "Static pages" do
       expect(page).to have_title("TalkVite | About")
     end
   end
+
+ describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+  end
+
 end
