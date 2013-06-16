03505ca785501baac60f1ae79705a76a11b65fda
jashmead <john.ashmead@ashmeadsoftware.com>
2013-06-16 18:10:17 -0400

    added in line config.assets.precompile += %w(custom.css hover.css users.css works.css styleie9.css)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,8 @@ Talkvite::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w(custom.css hover.css users.css works.css styleie9.css)
+  config.assets.precompile += %w( search.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
