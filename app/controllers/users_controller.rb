21f7de56be098c895d17f9368fdc6770eaa19246
jashmead <john.ashmead@ashmeadsoftware.com>
2013-07-09 13:23:51 +0100

    worked thru to end of chapter 7 of the tutorial (sign-up)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
   end
@@ -10,9 +11,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+	  # save message for use in next screen
       flash[:success] = "Welcome to TalkVite!"
+	  # how does this redirect_to work?
       redirect_to @user # don't need the user_url!
     else
+	  # error message from flash, will have been setup by 'save' method
       render 'new'
     end
   end
@@ -20,6 +24,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
+	  # user_params called in 'create'
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
